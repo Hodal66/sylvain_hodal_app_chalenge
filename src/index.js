a9af05213bb1788917fa7843bbe8051eb8afd548
niyonkuru1 <sylvainniyonkuru2@gmail.com>
2022-10-10 22:11:24 +0200

    git init

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import "./index.css";
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink } from "@apollo/client";
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://localhost:4000' }),
+  fetchOptions: {
+    mode: 'no-cors',
+  },
+  cache: new InMemoryCache()
+});
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
--- src/pages/allcountriesPage/AllCountries.jsx
+++ src/pages/allcountriesPage/AllCountries.jsx
@@ -1,17 +1,92 @@
-import React from "react";
+import React, { useEffect, useState, CSSProperties } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
 import Header from "../../components/Header";
 import HeaderImage from "../../images/header_baground1 1.png";
 import "./allCountries.css";
 import Countries from "../../components/Countries";
 import countries from "../../data";
 
+import { gql, useQuery } from "@apollo/client";
+
+export const GET_ALL_COUNTRIES = gql`
+  query {
+    getAllCountries {
+      code
+      name
+      native
+      phone
+      currency
+      emoji
+      emojiU
+      continent {
+        code
+        name
+      }
+      languages {
+        code
+        native
+        name
+        rtl
+      }
+      states {
+        code
+        name
+      }
+      images {
+        date_posted
+        image_url
+        _id
+        author {
+          email
+          firstName
+          secondName
+          _id
+        }
+      }
+      comments {
+        author {
+          email
+          firstName
+          secondName
+          _id
+        }
+        date_posted
+        content
+        _id
+      }
+      _id
+    }
+  }
+`;
+
+const override = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "red",
+};
 function AllCountries() {
+    const { error, data, loading } = useQuery(GET_ALL_COUNTRIES);
+  
+    console.log(data)
+    const color = "yellow";
   return (
     <div>
-      <Header title="Our Trainers " image={HeaderImage}>
-        Now Enjoy New world by exploring by searching your prefered country and
-        exploring All country library you are welcome🏹🚀
-      </Header>
+      <ClipLoader
+        color={color}
+        loading={loading}
+        cssOverride={override}
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+      {loading ? (
+        <div>Data is loading .....</div>
+      ) : (
+        <Header title="Our Trainers " image={HeaderImage}>
+          Now Enjoy New world by exploring by searching your prefered country
+          and exploring All country library you are welcome🏹🚀
+        </Header>
+      )}
       <section>
         <div className="search_container">
           <input
